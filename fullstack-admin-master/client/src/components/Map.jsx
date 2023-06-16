144ecd6ff71eb58beae5d903441fc236a192e128
Killian McShane <killianmcshane@Killians-MacBook-Pro.local>
2023-06-15 17:30:51 -0700

    Updated Map component to include Google Maps API. API key is defined in public/index.html but can also be done in the Map component itself.

--- fullstack-admin-master/client/src/components/Map.jsx
+++ fullstack-admin-master/client/src/components/Map.jsx
@@ -1,104 +1,91 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, TextField, Slider, Button, Box, useTheme } from "@mui/material";
-import { ConstructionRounded, RestartAlt, SearchOutlined } from '@mui/icons-material';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
-import GoogleMapReact from 'google-map-react';
-import Marker from "components/Marker";
+import React, { useEffect, useRef } from 'react';
 
-const Map = () => {
-    const theme = useTheme();
-    const [location, setLocation] = useState({
-        latitude: 24.723456,
-        longitude: 46.70095,
-    });
-    const [mocks, setMocks] = useState([]);
-    const [selectedMockId, setSelectedMockId] = useState(null);
-    const [searchText, setSearchText] = useState("");
-    { /* Need to define and assign polylinePath from props */ } 
-    const [polylinePath, setPolylinePath] = useState([]);
-    
-    { /* Mock polylinePath data
-    const polylinePath = [
-    { lat: 41.879, lng: -87.624 },
-    { lat: 41.878, lng: -87.629 },
-    ];
-    */ }
+// Replace the path prop with actual data
+const Map = ({ path }) => {
+    const mapRef = useRef(null);
+    const markersRef = useRef([]);
+    const polylineRef = useRef(null);
 
-    { /* Handles search for a known list of ids
-    const handleSearch = () => {
-        let filteredMocks = mockData.filter(x => x.name.toLowerCase().includes(searchText.toLowerCase()))
-        setMocks(filteredMocks);
-    }
-    */ }
+    const handleKeyDown = (event) => {
+        // Check if the target of the event is a marker
+        if (event.target.markerIndex !== undefined) {
+            let newIndex;
+
+            // Calculate the new index based on the key that was pressed
+            if (event.key === 'Enter' || event.key === ' ') {
+                console.log("Pressed Enter");
+                { /* handleMarkerClick(); */ }
+            } else if (event.key === 'ArrowRight') {
+                event.preventDefault();
+                newIndex = (event.target.markerIndex + 1) % markersRef.current.length;
+            } else if (event.key === 'ArrowLeft') {
+                event.preventDefault();
+                newIndex = (event.target.markerIndex - 1 + markersRef.current.length) % markersRef.current.length;
+            }
+
+            // Set the focus to the new marker
+            markersRef.current[newIndex].setFocus();
+        }
+    };
+
+    const initMap = () => {
+        const map = new window.google.maps.Map(mapRef.current, {
+            center: { lat: 10.99835602, lng: 77.01502627 },
+            zoom: 14,
+        });
+
+        // Create markers and polyline
+        markersRef.current = path.map((point, index) => {
+            const marker = new window.google.maps.Marker({
+                position: point,
+                map,
+                title: `#${index + 1}`,
+            });
+
+            marker.markerIndex = index;
+
+            // Add a click listener to focus the marker when it's clicked
+            marker.addListener('click', () => {
+                marker.setFocus();
+            });
+
+            return marker;
+        });
+
+        polylineRef.current = new window.google.maps.Polyline({
+            path,
+            geodesic: true,
+            strokeColor: "#ff2527",
+            strokeOpacity: 0.75,
+            strokeWeight: 2,
+            map,
+        });
+
+        // Clean up on unmount
+        return () => {
+            markersRef.current.forEach(marker => marker.setMap(null));
+            markersRef.current = [];
+            polylineRef.current.setMap(null);
+            polylineRef.current = null;
+        };
+    };
 
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                console.log(position.coords);
-                setLocation({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                });
-            },
-            (error) => {
-                console.log("Error Getting Location: " + error.message);
-            }
-        );
-    }, []);
-    
-    return (
-        <div>
-            <TextField label="Search for a supplier..."
-                variant="outlined"
-                style={{ width: "40%" }}
-                onChange={(event) => setSearchText(event.target.value)}
-            />
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap`;
+        script.async = true;
+        window.initMap = initMap;
+        document.body.appendChild(script);
 
-            <div>
-                <Button variant="contained" style={{ width: "20%" }}>
-                    <RestartAlt />
-                    Reset
-                </Button>
-                <Button variant="contained"
-                //  onClick={handleSearch}
-                    style={{ width: "20%" }}>
-                    <SearchOutlined />
-                    Search
-                </Button>
-            </div>
+        window.addEventListener('keydown', handleKeyDown);
 
-            <Box mt="2rem">
-                <div style={{ height: "80vh", width: "100%" }}>
-                    <GoogleMapReact
-                        bootstrapURLKeys={{ key: "AIzaSyAuVjIdVnypBE451-sxt-h-_R78hQSUDPI" }}
-                        defaultCenter={{
-                            lat: 10.99835602,
-                            lng: 77.01502627
-                        }}
-                        defaultZoom={14}
-                    >
-                        { /* NEED TO REPLACE mocks and mock */ }
-                        {
-                            mocks.map((mock, i) => {
-                                return (
-                                    <Marker
-                                        lat={mock.latitude}
-                                        lng={mock.longitude}
-                                        onClick={() => setSelectedMockId(mock.id)}
-                                        title={`${i + 1}. ${mock.name}`} // replace mock.name with the appropriate field
-                                        index={i}
-                                        totalMarkers={mocks.length}
-                                    />
-                                )
-                            })
-                        }
-                        <MapPolyline path={polylinePath} />
-                    </GoogleMapReact>
-                </div>
-            </Box>
-        </div>
-    );
-  };
-  
-  export default Map;
+        return () => {
+            // Clean up the event listener when the component unmounts
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [path]);
+
+    return <div ref={mapRef} style={{ height: "80vh", width: "100%" }} />;
+};
+
+export default Map;
