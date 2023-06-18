0622659a5a579f2dcc7f7afb001df12f729e4a20
Vikram Waradpande <vikram.waradpande@alethx.com>
2023-06-17 23:08:53 -0700

    changes for new data for hackathon

--- fullstack-admin-master/server/data/index.js
+++ fullstack-admin-master/server/data/index.js
@@ -18489,219 +18489,469 @@ export const dataTransaction = [
 //     "next": "PT2023019SK76D4"
 //   }];
 
+// export const dataShipments = [
+//   { // 1
+//     "id": "TR2022299HPQ6NS",
+//     "name": "11M ÇELİK İNŞAAT SAN. VE TİC. LTD. ŞTİ.",
+//     "coordinates": [
+//       24.6180732,
+//       23.4507856
+//     ],
+//     "material": "Synthetic",
+//     "amount": 65.1,
+//     "unit": "piece",
+//     "prev": [],
+//     "next": "PT2023018Q22FVH",
+//     "shipmentID": "Shipment1"
+//   },
+//   { // 2
+//     "id": "PT2023018Q22FVH",
+//     "name": "13 STRINGS, LDA",
+//     "coordinates": [
+//       17.2064912,
+//       22.1782433
+//     ],
+//     "material": "Cotton",
+//     "amount": 23.78,
+//     "unit": "kg",
+//     "prev": ["TR2022299HPQ6NS"],
+//     "next": "US202220707WWRM",
+//     "shipmentID": "Shipment1"
+//   },
+//   { // 3
+//     "id": "US202220707WWRM",
+//     "name": "100% WOOL INC",
+//     "coordinates": [
+//       24.1778298,
+//       20.1569574
+//     ],
+//     "material": "Silk",
+//     "amount": 78.22,
+//     "unit": "lb",
+//     "prev": ["PT2023018Q22FVH"],
+//     "next": "NM202220707WWRM",
+//     "shipmentID": "Shipment1"
+//   },
+//   { // 4
+//     "id": "NM202220707WWRM",
+//     "name": "Cool Shirt! INC",
+//     "coordinates": [
+//       22.1778298,
+//       25.1569574
+//     ],
+//     "material": "Silk",
+//     "amount": 78.22,
+//     "unit": "lb",
+//     "prev": ["US202220707WWRM"],
+//     "next": "BO202320707WWRM",
+//     "shipmentID": "Shipment1"
+//   },
+//   { // 5
+//     "id": "BO202320707WWRM",
+//     "name": "Clover Clothes INC",
+//     "coordinates": [
+//       29.1778298,
+//       35.1569574
+//     ],
+//     "material": "Silk",
+//     "amount": 78.22,
+//     "unit": "lb",
+//     "prev": ["NM202220707WWRM"],
+//     "next": "",
+//     "shipmentID": "Shipment1"
+//   },
+//   { // 1
+//     "id": "CN2022364N63PG",
+//     "name": "1001 ARMED FORCES CO LTD",
+//     "coordinates": [
+//       18.9786636,
+//       16.4464722
+//     ],
+//     "material": "Denim",
+//     "amount": 56.3,
+//     "unit": "piece",
+//     "prev": [],
+//     "next": "CN2021336XXCHG",
+//     "shipmentID": "Shipment2"
+//   },
+//   { // 2
+//     "id": "CN2021336XXCHG",
+//     "name": "10TH BRANCH OF GUANGZHOU FANGYING JEWELRY CO LTD",
+//     "coordinates": [
+//       17.1651282,
+//       17.6351594
+//     ],
+//     "material": "Wool",
+//     "amount": 45.78,
+//     "unit": "kg",
+//     "prev": ["CN2022364N63PG"],
+//     "next": "US2023019X1TH4L",
+//     "shipmentID": "Shipment2"
+//   },
+//   { // 3
+//     "id": "US2023019X1TH4L",
+//     "name": "10TH STREET GYM, LLC",
+//     "coordinates": [
+//       18.7714388,
+//       16.8150366
+//     ],
+//     "material": "Leather",
+//     "amount": 89.32,
+//     "unit": "lb",
+//     "prev": ["CN2021336XXCHG"],
+//     "next": "",
+//     "shipmentID": "Shipment2"
+//   },
+//   { // 4
+//     "id": "BNG023019X1TH4L",
+//     "name": "11TH STREET Fashion, LLC",
+//     "coordinates": [
+//       17.7714388,
+//       13.8150366
+//     ],
+//     "material": "Leather",
+//     "amount": 89.32,
+//     "unit": "lb",
+//     "prev": ["CN2021336XXCHG"],
+//     "next": "YYE2023019X1TH4L",
+//     "shipmentID": "Shipment2"
+//   },
+//   { // 5
+//     "id": "YYE2023019X1TH4L",
+//     "name": "Driponomics Major Clothing, LLC",
+//     "coordinates": [
+//       14.7714388,
+//       15.8150366
+//     ],
+//     "material": "Leather",
+//     "amount": 89.32,
+//     "unit": "lb",
+//     "prev": ["BNG023019X1TH4L"],
+//     "next": "",
+//     "shipmentID": "Shipment2"
+//   },
+//   { // 1
+//     "id": "PT2023019ZP6R9J",
+//     "name": "14WE, UNIPESSOAL, LDA",
+//     "coordinates": [
+//       24.1462883,
+//       15.530527
+//     ],
+//     "material": "Cotton",
+//     "amount": 123.21,
+//     "unit": "kg",
+//     "prev": [],
+//     "next": "US2023019X1V4J5",
+//     "shipmentID": "Shipment3"
+//   },
+//   { // 2
+//     "id": "US2023019X1V4J5",
+//     "name": "10TH FABRIC MEDICAL",
+//     "coordinates": [
+//       23.0028753,
+//       18.8600144
+//     ],
+//     "material": "Cotton",
+//     "amount": 321.45,
+//     "unit": "lb",
+//     "prev": ["PT2023019ZP6R9J"],
+//     "next": "US2023019Z2JT02",
+//     "shipmentID": "Shipment3"
+//   },
+//   { // 3
+//     "id": "US2023019Z2JT02",
+//     "name": "808, INC.",
+//     "coordinates": [
+//       22.9924469,
+//       20.1272764
+//     ],
+//     "material": "Denim",
+//     "amount": 123.45,
+//     "unit": "piece",
+//     "prev": ["US2023019X1V4J5"],
+//     "next": "EW2023019Z2JT02",
+//     "shipmentID": "Shipment3"
+//   },
+//   { // 4
+//     "id": "EW2023019Z2JT02",
+//     "name": "101 Style, INC.",
+//     "coordinates": [
+//       21.0024469,
+//       19.1272764
+//     ],
+//     "material": "Denim",
+//     "amount": 123.45,
+//     "unit": "piece",
+//     "prev": ["US2023019Z2JT02"],
+//     "next": "REW023019Z2JT02",
+//     "shipmentID": "Shipment3"
+//   },
+//   { // 5
+//     "id": "REW023019Z2JT02",
+//     "name": "Wearable Thread, INC.",
+//     "coordinates": [
+//       24.9924469,
+//       22.1272764
+//     ],
+//     "material": "Denim",
+//     "amount": 123.45,
+//     "unit": "piece",
+//     "prev": ["EW2023019Z2JT02"],
+//     "next": "",
+//     "shipmentID": "Shipment3"
+//   },
+//   { // 1
+//     "id": "TR2022299HPQ6NT",
+//     "name": "Yes Clothing. VE TİC. LTD. ŞTİ.",
+//     "coordinates": [
+//       22.6400492,
+//       23.9828272
+//     ],
+//     "material": "Synthetic",
+//     "amount": 65.1,
+//     "unit": "piece",
+//     "prev": [""],
+//     "next": "PT2023018Q22FVH1",
+//     "shipmentID": "Shipment4"
+//   },
+//   { // 2
+//     "id": "PT2023018Q22FVH1",
+//     "name": "16 STRINGS, LDA",
+//     "coordinates": [
+//       25.5612545,
+//       19.212993
+//     ],
+//     "material": "Cotton",
+//     "amount": 23.78,
+//     "unit": "kg",
+//     "prev": ["TR2022299HPQ6NT"],
+//     "next": "US202220707WWRM1",
+//     "shipmentID": "Shipment4"
+//   },
+//   { // 3
+//     "id": "US202220707WWRM1",
+//     "name": "Comfy Clothing INC",
+//     "coordinates": [
+//       20.9038371,
+//       14.2978584
+//     ],
+//     "material": "Silk",
+//     "amount": 78.22,
+//     "unit": "lb",
+//     "prev": ["PT2023018Q22FVH1"],
+//     "next": "PAN02220707WWRM1",
+//     "shipmentID": "Shipment4"
+//   },
+//   { // 4
+//     "id": "PAN02220707WWRM1",
+//     "name": "The Best Apparel INC",
+//     "coordinates": [
+//       16.9038371,
+//       14.2978584
+//     ],
+//     "material": "Silk",
+//     "amount": 78.22,
+//     "unit": "lb",
+//     "prev": ["US202220707WWRM1"],
+//     "next": "EC202220707WWRM1",
+//     "shipmentID": "Shipment4"
+//   },
+//   { // 5
+//     "id": "EC202220707WWRM1",
+//     "name": "Sustainable Garments INC",
+//     "coordinates": [
+//       15.9038371,
+//       19.2978584
+//     ],
+//     "material": "Silk",
+//     "amount": 78.22,
+//     "unit": "lb",
+//     "prev": ["PAN02220707WWRM1"],
+//     "next": "",
+//     "shipmentID": "Shipment4"
+//   },
+//   { // 1
+//     "id": "CN2022364N63PG1",
+//     "name": "1001 NAVAL FORCES CO LTD",
+//     "coordinates": [
+//       28.6883354,
+//       10.4599867
+//     ],
+//     "material": "Denim",
+//     "amount": 56.3,
+//     "unit": "piece",
+//     "prev": [],
+//     "next": "CN2021336XXCHG1",
+//     "shipmentID": "Shipment5"
+//   },
+//   { // 2
+//     "id": "CN2021336XXCHG1",
+//     "name": "5TH BRANCH OF GUANGZHOU FANGYING FASHION CO LTD",
+//     "coordinates": [
+//       8.1612447,
+//       24.4597336
+//     ],
+//     "material": "Wool",
+//     "amount": 45.78,
+//     "unit": "kg",
+//     "prev": ["CN2022364N63PG1"],
+//     "next": "US2023019X1TH4L1",
+//     "shipmentID": "Shipment5"
+//   },
+//   { // 3
+//     "id": "US2023019X1TH4L1",
+//     "name": "10TH STREET GYM, LLC",
+//     "coordinates": [
+//       18.7714388,
+//       16.8150366
+//     ],
+//     "material": "Leather",
+//     "amount": 89.32,
+//     "unit": "lb",
+//     "prev": ["CN2021336XXCHG1"],
+//     "next": "RAN032019X1TH4L1",
+//     "shipmentID": "Shipment5"
+//   },
+//   { // 4
+//   "id": "RAN032019X1TH4L1",
+//   "name": "19TH STREET GYM, LLC",
+//   "coordinates": [
+//     16.7714388,
+//     16.3250366
+//   ],
+//   "material": "Leather",
+//   "amount": 89.32,
+//   "unit": "lb",
+//   "prev": ["US2023019X1TH4L1"],
+//   "next": "YUF023019X1TH4L1",
+//   "shipmentID": "Shipment5"
+// },
+// { // 5
+// "id": "YUF023019X1TH4L1",
+// "name": "THE SPOT APPAREL, LLC",
+// "coordinates": [
+//   14.0814388,
+//   12.8150366
+// ],
+// "material": "Leather",
+// "amount": 89.32,
+// "unit": "lb",
+// "prev": ["RAN032019X1TH4L1"],
+// "next": "",
+// "shipmentID": "Shipment5"
+// }
+// ];
+
 export const dataShipments = [
   {
-    "id": "TR2022299HPQ6NS",
-    "name": "11M ÇELİK İNŞAAT SAN. VE TİC. LTD. ŞTİ.",
-    "coordinates": [
-      24.6180732,
-      23.4507856
-    ],
-    "material": "Synthetic",
-    "amount": 65.1,
-    "unit": "piece",
-    "prev": ["PT2023019Z2JT02"],
-    "next": "",
-    "shipmentID": "Shipment1"
+    id: 'US2020349C9G8HT',
+    coordinates: [ -93.5448808, 45.2270374 ],
+    name: '10K Lettering',
+    country_code: 'US'
   },
   {
-    "id": "PT2023018Q22FVH",
-    "name": "13 STRINGS, LDA",
-    "coordinates": [
-      17.2064912,
-      22.1782433
-    ],
-    "material": "Cotton",
-    "amount": 23.78,
-    "unit": "kg",
-    "prev": [],
-    "next": "US202220707WWRM",
-    "shipmentID": "Shipment1"
+    id: 'US2020349635F81',
+    coordinates: [ -84.7767657, 37.629886 ],
+    name: '10th Planet LLC-The',
+    country_code: 'US'
   },
   {
-    "id": "US202220707WWRM",
-    "name": "100% WOOL INC",
-    "coordinates": [
-      24.1778298,
-      20.1569574
-    ],
-    "material": "Silk",
-    "amount": 78.22,
-    "unit": "lb",
-    "prev": ["PT2023018Q22FVH"],
-    "next": "",
-    "shipmentID": "Shipment1"
+    id: 'US2022300WRE2C1',
+    coordinates: [ -81.417946, 28.428796 ],
+    name: '11th Hour Business Solutions',
+    country_code: 'US'
   },
   {
-    "id": "CN2022364N63PG",
-    "name": "1001 ARMED FORCES CO LTD",
-    "coordinates": [
-      18.9786636,
-      16.4464722
-    ],
-    "material": "Denim",
-    "amount": 56.3,
-    "unit": "piece",
-    "prev": [],
-    "next": "",
-    "shipmentID": "Shipment2"
+    id: 'DE2023019MMW839',
+    coordinates: [ 6.4473648, 51.182841 ],
+    name: '140Fahrenheit GmbH',
+    country_code: 'DE'
   },
   {
-    "id": "CN2021336XXCHG",
-    "name": "10TH BRANCH OF GUANGZHOU FANGYING JEWELRY CO LTD",
-    "coordinates": [
-      17.1651282,
-      17.6351594
-    ],
-    "material": "Wool",
-    "amount": 45.78,
-    "unit": "kg",
-    "prev": [],
-    "next": "US2023019X1TH4L",
-    "shipmentID": "Shipment2"
+    id: 'DE20223002JTQC3',
+    coordinates: [ 11.1692099, 49.3523669 ],
+    name: '2M Holdings Limited',
+    country_code: 'DE'
   },
   {
-    "id": "US2023019X1TH4L",
-    "name": "10TH STREET GYM, LLC",
-    "coordinates": [
-      18.7714388,
-      16.8150366
-    ],
-    "material": "Leather",
-    "amount": 89.32,
-    "unit": "lb",
-    "prev": ["CN2021336XXCHG"],
-    "next": "",
-    "shipmentID": "Shipment2"
+    id: 'DE202118936ZXHP',
+    coordinates: [ 9.1713135, 47.6647294 ],
+    name: '3FREUNDE',
+    country_code: 'DE'
   },
   {
-    "id": "PT2023019ZP6R9J",
-    "name": "14WE, UNIPESSOAL, LDA",
-    "coordinates": [
-      24.1462883,
-      15.530527
-    ],
-    "material": "Cotton",
-    "amount": 123.21,
-    "unit": "kg",
-    "prev": [],
-    "next": "US2023019X1V4J5",
-    "shipmentID": "Shipment3"
+    id: 'FR2023034Z4W4Q0',
+    coordinates: [ 2.405313, 48.8971575 ],
+    name: '205244-IBOUTEX-MIKO MODE',
+    country_code: 'FR'
   },
   {
-    "id": "US2023019X1V4J5",
-    "name": "10TH STREET MEDICAL",
-    "coordinates": [
-      23.0028753,
-      18.8600144
-    ],
-    "material": "Cotton",
-    "amount": 321.45,
-    "unit": "lb",
-    "prev": ["PT2023019ZP6R9J"],
-    "next": "",
-    "shipmentID": "Shipment3"
+    id: 'FR20222287Q0ZBJ',
+    coordinates: [ 2.213749, 46.227638 ],
+    name: '2XL France',
+    country_code: 'FR'
   },
   {
-    "id": "US2023019Z2JT02",
-    "name": "111, INC.",
-    "coordinates": [
-      22.9924469,
-      20.1272764
-    ],
-    "material": "Denim",
-    "amount": 123.45,
-    "unit": "piece",
-    "prev": [],
-    "next": "TR2022299HPQ6NT",
-    "shipmentID": "Shipment3"
+    id: 'FR20223007P6T97',
+    coordinates: [ -1.7848761, 46.5227278 ],
+    name: '37 Deux',
+    country_code: 'FR'
   },
   {
-    "id": "TR2022299HPQ6NT",
-    "name": "11M ÇELİK İNŞAAT SAN. VE TİC. LTD. ŞTİ.",
-    "coordinates": [
-      22.6400492,
-      23.9828272
-    ],
-    "material": "Synthetic",
-    "amount": 65.1,
-    "unit": "piece",
-    "prev": ["US2023019Z2JT02"],
-    "next": "",
-    "shipmentID": "Shipment4"
+    id: 'EG20211894J5X61',
+    coordinates: [ 31.3909597, 30.2327128 ],
+    name: 'ACE Apparel Egypt',
+    country_code: 'EG'
   },
   {
-    "id": "PT2023018Q22FVH1",
-    "name": "13 STRINGS, LDA",
-    "coordinates": [
-      25.5612545,
-      19.212993
-    ],
-    "material": "Cotton",
-    "amount": 23.78,
-    "unit": "kg",
-    "prev": [],
-    "next": "US202220707WWRM1",
-    "shipmentID": "Shipment4"
+    id: 'EG201909658XSER',
+    coordinates: [ 31.2622119, 30.0793901 ],
+    name: 'AGE (Al Shimaa For Garment And Embroidery)',
+    country_code: 'EG'
   },
   {
-    "id": "US202220707WWRM1",
-    "name": "100% WOOL INC",
-    "coordinates": [
-      20.9038371,
-      14.2978584
-    ],
-    "material": "Silk",
-    "amount": 78.22,
-    "unit": "lb",
-    "prev": ["PT2023018Q22FVH1"],
-    "next": "",
-    "shipmentID": "Shipment4"
+    id: 'EG2020079QKASK0',
+    coordinates: [ 31.0358045, 30.4396164 ],
+    name: 'Ahmed Ahmed Abbas Aggour for Readymade Garments',
+    country_code: 'EG'
   },
   {
-    "id": "CN2022364N63PG1",
-    "name": "1001 ARMED FORCES CO LTD",
-    "coordinates": [
-      28.6883354,
-      10.4599867
-    ],
-    "material": "Denim",
-    "amount": 56.3,
-    "unit": "piece",
-    "prev": [],
-    "next": "",
-    "shipmentID": "Shipment5"
+    id: 'RU20223000D0AHR',
+    coordinates: [ 37.5807921, 55.7477968 ],
+    name: 'AMD International Sales & Services, Ltd.',
+    country_code: 'RU'
   },
   {
-    "id": "CN2021336XXCHG1",
-    "name": "10TH BRANCH OF GUANGZHOU FANGYING JEWELRY CO LTD",
-    "coordinates": [
-      8.1612447,
-      24.4597336
-    ],
-    "material": "Wool",
-    "amount": 45.78,
-    "unit": "kg",
-    "prev": [],
-    "next": "US2023019X1TH4L1",
-    "shipmentID": "Shipment5"
+    id: 'RU2022300ADC32R',
+    coordinates: [ 38.1298384, 53.1042919 ],
+    name: 'Cargill Incorporated',
+    country_code: 'RU'
   },
   {
-    "id": "US2023019X1TH4L1",
-    "name": "10TH STREET GYM, LLC",
-    "coordinates": [
-      18.7714388,
-      16.8150366
-    ],
-    "material": "Leather",
-    "amount": 89.32,
-    "unit": "lb",
-    "prev": ["CN2021336XXCHG1"],
-    "next": "",
-    "shipmentID": "Shipment5"
+    id: 'RU2022300ZMRH0C',
+    coordinates: [ 30.2713058, 60.04490329999999 ],
+    name: 'Certain bvba',
+    country_code: 'RU'
+  },
+  {
+    id: 'MX2019100Y2CPN9',
+    coordinates: [ -103.4737412, 25.5574353 ],
+    name: 'Aalfs Mfg Mexico OMJC',
+    country_code: 'MX'
+  },
+  {
+    id: 'MX2019100SP5PA6',
+    coordinates: [ -103.442293, 26.1056235 ],
+    name: 'Aalfs OMJC Tlahualilo Sew',
+    country_code: 'MX'
+  },
+  {
+    id: 'MX2021125QNKGST',
+    coordinates: [ -101.6733425, 21.1089089 ],
+    name: 'ABUCOMBAL',
+    country_code: 'MX'
   }
 ];
-
 
 export const dataUser = [
   {
