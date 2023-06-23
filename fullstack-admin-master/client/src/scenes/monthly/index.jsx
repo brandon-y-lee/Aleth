4a7c3a07097f0b75a9730c19b034481b9787eb8e
KillianAlethX <134575578+KillianAlethX@users.noreply.github.com>
2023-06-22 17:03:21 -0700

    Update index.jsx with FileUpload Implementation

--- fullstack-admin-master/client/src/scenes/monthly/index.jsx
+++ fullstack-admin-master/client/src/scenes/monthly/index.jsx
@@ -41,6 +41,9 @@ const Monthly = () => {
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="MONTHLY SALES" subtitle="Chart of monthlysales" />
+      
+      <FileUpload />
+      
       <Box height="75vh">
         {data ? (
           <ResponsiveLine
