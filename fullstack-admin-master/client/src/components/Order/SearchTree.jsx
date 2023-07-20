d2760be1b3ecd4e2eb5d95491c8c9ee67b7e19aa
Vikram Waradpande <vikram.waradpande@alethx.com>
2023-07-20 16:06:17 -0700

    fix search tree with the correct query to seller details

--- fullstack-admin-master/client/src/components/Order/SearchTree.jsx
+++ fullstack-admin-master/client/src/components/Order/SearchTree.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import ReactEChartsCore from 'echarts-for-react/lib/core';
 import * as echarts from 'echarts/core';
 import { TooltipComponent } from 'echarts/components';
@@ -10,6 +10,16 @@ echarts.use([TooltipComponent, TreeChart, CanvasRenderer]);
 const SearchTree = ({ techPackId }) => {
   const {data: techPack, isLoading: isLoadingTechPack} = useGetTechPackQuery({techPackId});
   const {data: queries, isLoading: isLoadingQueries} = useGetQueriesForTechPackQuery({techPackId});
+  const [selectedQuery, setSelectedQuery] = useState(null);
+
+  const {data: sellers} = useGetEligibleSellersAdvancedQuery({
+    products: [selectedQuery?.productCategory],
+    material: selectedQuery?.materialType,
+    fabricConstruction: selectedQuery?.fabricConstruction,
+    certifications: ""  // Adjust this as needed
+  });
+
+  console.log(sellers);
 
   const [option, setOption] = useState({
     tooltip: {
@@ -47,6 +57,16 @@ const SearchTree = ({ techPackId }) => {
     ],
   });
 
+  const onChartClick = useCallback((params) => {
+    // console.log(params);
+    // console.log(queries.techPackQueries.find(query => query.id === "1"));
+    if (params.dataType === 'main') {
+      const query = queries.techPackQueries.find(query => query.id === params.data.id);
+      console.log(query);
+      setSelectedQuery(query);
+    }
+  }, [queries]);
+
   useEffect(() => {
     if (isLoadingTechPack || isLoadingQueries) return;
 
@@ -54,38 +74,54 @@ const SearchTree = ({ techPackId }) => {
       queries.techPackQueries.find(query => query.id === queryId)
     );
 
-    Promise.all(
-      actualQueries.map((query) =>
-        useGetEligibleSellersAdvancedQuery({
-          products: query.productCategory,
-          material: query.materialType,
-          fabricConstruction: query.fabricConstruction,
-          certifications: ""  // Adjust this as needed
-        }).then(sellers => ({ query, sellers }))
-      )
-    ).then(allEligibleSellers => {
-      const children = allEligibleSellers.map(({ query, sellers }) => {
-        const sellerNodes = sellers.map(seller => ({ name: seller.name }));
-        return {
-          name: query.material,
-          children: sellerNodes,
-        };
-      });
+    const sellers = [];
+    const treeData = {
+      name: techPack.techPack.productCategory,
+      children: actualQueries.map((query) => ({
+        name: query.material,
+        "id": query.id
+        // children: sellers.map(seller => ({ name: seller.name, id:  })),
+      })),
+    };
 
-      const treeData = {
-        name: techPack.techPack.productCategory,
-        children,
-      };
-
-      setOption(prevOption => ({
-        ...prevOption,
-        series: [{
-          ...prevOption.series[0],
+    setOption({
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove',
+      },
+      series: [
+        {
+          type: 'tree',
+          top: '1%',
+          left: '7%',
+          bottom: '1%',
+          right: '20%',
+          symbolSize: 7,
+          label: {
+            position: 'left',
+            verticalAlign: 'middle',
+            align: 'right',
+            fontSize: 9,
+          },
+          leaves: {
+            label: {
+              position: 'right',
+              verticalAlign: 'middle',
+              align: 'left',
+            },
+          },
+          emphasis: {
+            focus: 'descendant',
+          },
+          expandAndCollapse: true,
+          animationDuration: 550,
+          animationDurationUpdate: 750,
           data: [treeData],
-        }],
-      }));
+        },
+      ],
+      animationEasing: 'cubicOut',
     });
-  }, [techPackId, isLoadingTechPack, queries, isLoadingQueries]);
+  }, [techPackId, isLoadingTechPack, queries, isLoadingQueries, sellers]);
 
   return (
     <div style={{ height: '100%', width: '100%' }}>
@@ -94,6 +130,7 @@ const SearchTree = ({ techPackId }) => {
           echarts={echarts}
           option={option}
           style={{ height: '100%', width: '100%' }}
+          onEvents={{'click': onChartClick}}
         />
       )}
     </div>
