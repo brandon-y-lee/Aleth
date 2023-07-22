9dfe58163c786f089da07e4bb1f545202533fd25
brandon-y-lee <brandon@alethx.com>
2023-07-22 16:56:23 -0700

    Added license key for Syncfusion in client index.js

--- fullstack-admin-master/client/src/index.js
+++ fullstack-admin-master/client/src/index.js
@@ -7,9 +7,9 @@ import globalReducer from "state";
 import { Provider } from "react-redux";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "state/api";
-import Session from 'react-session-api'
+import {registerLicense} from "@syncfusion/ej2-base";
 
-// Session.set("username","2");
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF5cXmdCeUx0THxbf1xzZFJMZV9bRndPIiBoS35RdUVkW31feXFWRGFaVUZ/');
 
 const store = configureStore({
   reducer: {
